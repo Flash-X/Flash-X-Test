3f61d4623f5cdde0cbb1a4ff8663e0f9a3739983
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-01-31 11:19:20 -0600

    set version for future

--- FlashXTest/__meta__.py
+++ FlashXTest/__meta__.py
@@ -1,6 +1,6 @@
 """Metadata for FlashXTest package"""
 
 __pkgname__ = "FlashXTest"
-__version__ = "2023.02.a"
+__version__ = "2023.02"
 __authors__ = "The Flash-X Team"
 __license__ = "MIT License"
