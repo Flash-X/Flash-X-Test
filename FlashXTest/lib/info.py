7fc6cf5ae185e196112e49033afb05c8864fbde1
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-04-13 14:45:16 -0500

    bugfix

--- FlashXTest/lib/info.py
+++ FlashXTest/lib/info.py
@@ -227,7 +227,7 @@ def createInfo(mainDict, specList):
                 )
 
             if seedNode:
-                if seedNode.findChildrenWithPath(testSpec.nodeName):
+                if seedNode.findChildrenWithPath(testSpec.nodeName)[0]:
                     xmlText = seedNode.findChildrenWithPath(testSpec.nodeName)[0].text
                     for entries in xmlText:
                         if entries.split(":")[0] == "restartBenchmark":
