ba9f83946a4b0afd6192afa00b1f5eb3c8c45baa
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-09-25 14:19:32 -0500

    fix bug in hostname

--- FlashXTest/backend/FlashTest/flashTest.py
+++ FlashXTest/backend/FlashTest/flashTest.py
@@ -505,7 +505,7 @@ def main():
         try:
             FQHostname = socket.gethostbyaddr(socket.gethostname())[0]
         except:
-            FQHostName = "<HOSTNAME>"
+            FQHostname = "<HOSTNAME>"
 
         masterDict["FQHostname"] = FQHostname
     # Determine value of "site". This will be the first element of
