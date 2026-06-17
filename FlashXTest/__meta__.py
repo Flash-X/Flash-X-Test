69f8bffb15a6950a896de15f94b7e23ae4ccf8ea
akashdhruv <akashdhruv@gwmail.gwu.edu>
2026-06-17 15:41:00 -0700

    Update meta

--- FlashXTest/__meta__.py
+++ FlashXTest/__meta__.py
@@ -1,6 +1,6 @@
 """Metadata for FlashXTest package"""
 
 __pkgname__ = "FlashXTest"
-__version__ = "2025.02"
+__version__ = "2026.06.b"
 __authors__ = "The Flash-X Team"
 __license__ = "MIT License"
